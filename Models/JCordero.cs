79eba7fbb56f949af10d791888bcc39143fa8db4
laboratorio <laboratorio@UPOAULA10215>
2023-04-24 07:25:06 -0500

    Agregar archivos de proyecto.

---
+++ Models/JCordero.cs
@@ -0,0 +1,62 @@
+﻿namespace JCExamenP1.Models
+{
+    public class JCordero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*ConcurrencyCheck
+Key
+MaxLength
+Required
+StringLength
+Timestamp
+
+
+        https://udlaec-my.sharepoint.com/personal/joe_cordero_udla_edu_ec/_layouts/15/Doc.aspx?sourcedoc={093a5263-d4ed-456a-a60a-5817ec50886c}&action=edit&wd=target%28Semana%201.one%7C5417e711-8662-46e3-bd67-f57b44f97c53%2FP%C3%A1gina%20sin%20t%C3%ADtulo%7C5954330f-cab4-4f2b-ad8d-130bb0e7fe8b%2F%29&wdorigin=NavigationUrl
+
+
+        */
+    }
+}
